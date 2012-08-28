ff97e2b8609e690f88e0fca5760addcdb96dd4a3
Jennifer Witkowski <jwitkowski79@gmail.com>
2012-08-28 09:24:06 -0400

    Added new testimonials

--- portfolios.php
+++ portfolios.php
@@ -7,9 +7,28 @@
       <li><a href="contact.php">Contact Us</a></li>
     </ul>
         <div id="slideshow">
-   	<div><p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"</p> <p class="credit">first name Lastname <br/>Title<br/>Company</p></div><!--testimonial one-->
+   <div><p>"Buffalo Creative Group’s work is imaginative and they give you exactly what you require to make your business and products stand out."</p> <p class="credit">- Michele Hurley, PT <br/>  Chief Executive Officer<br/>  Move With Me Physical Therapy</p></div><!--testimonial one-->
    	
-   	<div><p>"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"</p> <p class="credit">first name Lastname2 <br/>Title<br/>Company</p></div><!--testimonial one-->
+   	<div><p>"Buffalo Creative Group provided an innovative and memorable logo design for our Real Estate user group. They produced a number of creative ideas, which captured the essence of our group, for us to choose from."</p> <p class="credit">Dave Hyzy <br/>Director of Information Technology<br/>Benderson Development Company</p></div><!--testimonial two-->
+   	
+   	 <div><p>"We truly appreciate the time and effort Buffalo Creative Group took to provide us with high-quality images in perfect accordance with the numerous details we asked for."</p> <p class="credit">Meg Lee<br/>Risk Management MDP<br/>M&T Bank</p></div><!--testimonial three-->
+   	 
+   	 <div><p>"It's like I have an extension of the marketing department. They are good listeners and their work always hits the target."</p> <p class="credit">Karen Carey<br/>Marketing Manager<br/>Great Lakes Orthodontics</p></div><!--testimonial four-->
+   	 
+   	 <div><p>"Buffalo Creative Group creates exactly what I need and always exceeds my expectations with beautiful designs that embody what I’m trying to  express to my clients!"</p> <p class="credit">Michele Hurley, PT<br/>Chief Executive Officer<br/>  Move With Me Physical Therapy</p></div><!--testimonial five-->
+   	 
+   	 <div><p>"I just saw the latest print - it’s absolutely exquisite - thank you thank you thank you. The brochure is so beautiful and inspiring...wholly due to your design work. Thank you so much"</p> <p class="credit">Megan Weaver<br/>Co-Founder<br/>  Revitalize Buffalo</p></div><!--testimonial six-->
+   	 
+   
+   <div><p>"Thanks again for your help. I truly appreciate your willingness to be flexible around my needs and desires. I’m honored and pleased to provide a reference for the wonderful work that you have done."</p> <p class="credit">Sandy Simon Kappelmann<br/>Owner<br/>  Zesty Life Coaching</p></div><!--testimonial seven-->
+   
+     <div><p>"Delivering on time and under budget, I would recommend Buffalo Creative Group to anyone looking for a uniquely creative collaboration."</p> <p class="credit">Dave Hyzy<br/>Director of Information Technology<br/>Benderson Development Company</p></div><!--testimonial eight-->
+     
+     <div><p>"LOVE the business cards. They are so cute! Everyone who has seen them is raving. Thanks again!"</p> <p class="credit">Katie Kahn<br/>Owner<br/>Butter Me Up Bakery</p></div><!--testimonial nine-->
+     
+     <div><p>"We are very impressed by your contributions and your dedication to providing graphics in a timely manner, despite our last minute requests. Thank you!"</p> <p class="credit">Meg Lee<br/>Risk Management MDP<br/>M&T Bank</p></div><!--testimonial ten-->
+     
+     <div><p>"They are more than designers...they are business partners. I know that I can rely on them to take ownership of any project and learn what is needed to understand and successfully execute what I am trying to achieve."</p> <p class="credit">Karen Carey<br/>Marketing Manager<br/>Great Lakes Orthodontics</p></div><!--testimonial eleven-->
    </div><!--/slideshow-->
 
     <!-- end .sidebar1 --></div>
